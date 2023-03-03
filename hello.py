da9cf8f5955d363a4ec75f64f6725492f9afc4b9
vardansaini <vardan1@ualberta.ca>
2023-03-03 15:06:22 -0700

    initial2

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/")
-def hello():
-    return "Hello, world!"
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, "/")
 
 
 if __name__ == "__main__":
